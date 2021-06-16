ba4707aa7cfd2e352353174c936db30b048aa982
kapavkin <kapavkin@edu.hse.ru>
2021-06-17 02:39:49 +0300

    remove build

--- build/adam/CMakeFiles/adam.dir/cmake_clean_target.cmake
+++
@@ -1,3 +0,0 @@
-file(REMOVE_RECURSE
-  "libadam.a"
-)
--- build/number/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/kirillpavkin/Downloads/laboratory-work-01-kapavkin/number
-# Build directory: /Users/kirillpavkin/Downloads/laboratory-work-01-kapavkin/build/number
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
